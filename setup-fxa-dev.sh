81d8b2884309f4fc0878b5a95ca79e58696e2312
Edouard Oger <eoger@fastmail.com>
2016-05-17 22:17:29 -0700

    Reduce verbosity of nvm commands

--- setup-fxa-dev.sh
+++ setup-fxa-dev.sh
@@ -5,11 +5,15 @@ set -e -v # Exit on error and print executed commands
 brew install gmp redis graphicsmagick
 sudo pip install virtualenv
 
+set +ev
+echo "source $HOME/.nvm/nvm.sh"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install 0.10
+CMD="nvm install 0.10"; echo $CMD; eval $CMD;
+CMD="nvm unalias default"; echo $CMD; eval $CMD
+set -ev
+
 npm install -g npm@2.4
-nvm unalias default
 
 git clone https://github.com/mozilla/fxa-local-dev.git "$HOME"/fxa/fxa-local-dev
 cd "$HOME"/fxa/fxa-local-dev
--- setup-node.sh
+++ setup-node.sh
@@ -2,7 +2,11 @@
 set -e -v # Exit on error and print executed commands
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+
+set +ev
+echo "source $HOME/.nvm/nvm.sh"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install node
-nvm unalias default
+CMD="nvm install node"; echo $CMD; eval $CMD;
+CMD="nvm unalias default"; echo $CMD; eval $CMD
+set -ev
