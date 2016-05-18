53af74f9f7f15ae23385e5ea9365e761171b2ab2
Edouard Oger <eoger@fastmail.com>
2016-05-17 22:48:06 -0700

    Do not redownload Package Control if it exists

--- setup-sublime.sh
+++ setup-sublime.sh
@@ -8,7 +8,7 @@ SUBLIME_CONFIG="$HOME"/Library/"Application Support"/"Sublime Text 3"
 SUBLIME_INSTALLED_PACKAGES="$SUBLIME_CONFIG"/"Installed Packages"
 SUBLIME_PACKAGES="$SUBLIME_CONFIG"/Packages
 mkdir -p "$SUBLIME_INSTALLED_PACKAGES"
-wget https://packagecontrol.io/Package%20Control.sublime-package -P "$SUBLIME_INSTALLED_PACKAGES"
+wget -nc https://packagecontrol.io/Package%20Control.sublime-package -P "$SUBLIME_INSTALLED_PACKAGES"
 
 mkdir -p "$SUBLIME_PACKAGES"
 ln -sf "$PWD"/sublime-text "$SUBLIME_PACKAGES"/User
