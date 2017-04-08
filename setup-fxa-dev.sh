ddd852002c33921bb69fa44d2f507d5585c94d68
Edouard Oger <eoger@fastmail.com>
2017-04-08 16:15:02 -0400

    Update FxA setup script

--- setup-fxa-dev.sh
+++ setup-fxa-dev.sh
@@ -9,13 +9,11 @@ set +ev
 echo "source $HOME/.nvm/nvm.sh"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-CMD="nvm install 0.10"; echo $CMD; eval $CMD;
+CMD="nvm install 4.8"; echo $CMD; eval $CMD;
 CMD="nvm unalias default"; echo $CMD; eval $CMD
 set -ev
 
-npm install -g npm@2.4
-
-git clone https://github.com/mozilla/fxa-local-dev.git "$HOME"/fxa/fxa-local-dev
-cd "$HOME"/fxa/fxa-local-dev
+git clone https://github.com/mozilla/fxa-local-dev.git "$HOME"/fxa
+cd "$HOME"/fxa
 npm i
 ./pm2 kill
