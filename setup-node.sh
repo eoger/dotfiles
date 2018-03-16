e4dbf777aafbd0d8177aedce8009baf1bcd612a4
Edouard Oger <eoger@fastmail.com>
2018-03-16 10:36:52 -0400

    Upgrade nvm install script

--- setup-node.sh
+++ setup-node.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e -v # Exit on error and print executed commands
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 set +ev
 echo "source $HOME/.nvm/nvm.sh"
